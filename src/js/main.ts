51acac2d61b3a20a38fc27b3ec0464ff82d2ef4f
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-09-10 13:26:34 -0700

    trying to draw card

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,9 @@
-const button : HTMLElement | null = document.getElementById("button");
+const CARD_HEIGHT = 100;
+const CARD_WIDTH = 50;
 
-button?.addEventListener(("click"), () => {
-    console.log("clicked");
-});
+const canvas : HTMLCanvasElement | null = document.getElementById("game") as HTMLCanvasElement;
+const ctx : CanvasRenderingContext2D | null = canvas?.getContext("2d");
+
+const img : HTMLImageElement = new Image(CARD_WIDTH, CARD_HEIGHT);
+img.src = "../resources/blue_0.png";
+ctx?.drawImage(img, 0, 0);
