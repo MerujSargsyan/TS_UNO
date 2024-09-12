f8ec662d9f67f160fe99d4bcf15d704dba8a85c2
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-09-11 18:21:34 -0700

    have to try to fix server stuff

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,9 +1,13 @@
 import Card from "./library.js";
 
-const body : HTMLBodyElement = (document.getElementsByTagName("body"))[0];
+const botArea : HTMLDivElement = document.getElementById("bot-area") as HTMLDivElement;
+const playerArea : HTMLDivElement = document.getElementById("player-area") as HTMLDivElement;
 
-let card = new Card(0, "blue");
+let card = new Card(0, "yellow");
 let img : HTMLImageElement = document.createElement("img"); 
+let img2 : HTMLImageElement = document.createElement("img"); 
 
 img.src = card.src;
-body.appendChild(img);
+img2.src = card.src;
+botArea.appendChild(img);
+playerArea.appendChild(img2);
