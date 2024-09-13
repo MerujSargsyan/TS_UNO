01a39f078f0af88192da6994bda27fd342d0d80d
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-09-12 18:10:45 -0700

    dealing cards

--- src/js/library.ts
+++ src/js/library.ts
@@ -8,3 +8,7 @@ export default class Card {
         this.src = `../resources/cards/${color}_${value}.png`;
     }
 }
+
+export const values : string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
+    "skip", "picker"];
+export const colors : string[] = ["red", "yellow", "green", "blue"];
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,13 +1,11 @@
 import Card from "./library.js";
+import { values, colors } from "./library.js";
 
 const botArea : HTMLDivElement = document.getElementById("bot-area") as HTMLDivElement;
 const playerArea : HTMLDivElement = document.getElementById("player-area") as HTMLDivElement;
 
 const deck : Card[] = [];
 
-const values : string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
-    "skip", "picker"];
-const colors : string[] = ["red", "yellow", "green", "blue"];
 
 for(let i = 0; i < 2; i++) {
     for(let val of values) {
@@ -22,8 +20,19 @@ for(let i = 0; i < 2; i++) {
     deck.push(new Card("draw4", "black"));
 }
 
-deck.forEach((card) => {
-    const cardImg : HTMLImageElement = document.createElement("img"); 
-    cardImg.src = card.src;
-    botArea.appendChild(cardImg);
-});
+const deal = () => {
+    for(let i = 0; i < 7; i++) {
+        const c : Card | undefined= deck.pop();
+        const img : HTMLImageElement = document.createElement("img");
+        img.src = c ? c.src : "";
+        botArea.appendChild(img);
+    }
+    for(let i = 0; i < 7; i++) {
+        const c : Card | undefined= deck.pop();
+        const img : HTMLImageElement = document.createElement("img");
+        img.src = c ? c.src : "";
+        playerArea.appendChild(img);
+    }
+}
+
+deal();
