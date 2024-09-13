d26bdeb5784175eede474b0029b040b24220710a
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-09-12 17:56:39 -0700

    drawing all images well

--- src/js/library.ts
+++ src/js/library.ts
@@ -1,8 +1,8 @@
 export default class Card {
-    value: number;
+    value: string;
     color: string;
     src: string;
-    constructor(value : number, color : string) {
+    constructor(value : string, color : string) {
         this.value = value;
         this.color = color;
         this.src = `../resources/cards/${color}_${value}.png`;
--- src/js/main.ts
+++ src/js/main.ts
@@ -3,11 +3,27 @@ import Card from "./library.js";
 const botArea : HTMLDivElement = document.getElementById("bot-area") as HTMLDivElement;
 const playerArea : HTMLDivElement = document.getElementById("player-area") as HTMLDivElement;
 
-let card = new Card(0, "yellow");
-let img : HTMLImageElement = document.createElement("img"); 
-let img2 : HTMLImageElement = document.createElement("img"); 
+const deck : Card[] = [];
 
-img.src = card.src;
-img2.src = card.src;
-botArea.appendChild(img);
-playerArea.appendChild(img2);
+const values : string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
+    "skip", "picker"];
+const colors : string[] = ["red", "yellow", "green", "blue"];
+
+for(let i = 0; i < 2; i++) {
+    for(let val of values) {
+        for (let col of colors) {
+            deck.push(new Card(val, col));
+        }
+    }
+
+    deck.push(new Card("wild", "black"));
+    deck.push(new Card("wild", "black"));
+    deck.push(new Card("draw4", "black"));
+    deck.push(new Card("draw4", "black"));
+}
+
+deck.forEach((card) => {
+    const cardImg : HTMLImageElement = document.createElement("img"); 
+    cardImg.src = card.src;
+    botArea.appendChild(cardImg);
+});
